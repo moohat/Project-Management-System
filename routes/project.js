5303c55eb44ad871e27cd5d7b682bcf3c20d8a86
moohat <refixzcorp@gmail.com>
2020-01-03 11:21:36 +0700

    pagination to project

--- routes/project.js
+++ routes/project.js
@@ -7,84 +7,84 @@ const helpers = require("../helpers/util")
 
 module.exports = function (pool) {
 
-    router.get("/", (req, res, next) => {
-        const page = req.query.page || 1;
-        const limit = 5;
-        const offset = (page - 1) * limit;
-        pool.query(`SELECT COUNT(*) AS total FROM projects  `, (err, response) => {
-            if (err) return res.send(err);
-            const pages = Math.ceil(response.rows[0].total / limit);
+
+    //todo: function get pagination only
+    // router.get("/", (req, res, next) => {
+    //     const page = req.query.page || 1;
+    //     const limit = 5;
+    //     const offset = (page - 1) * limit;
+    //     pool.query(`SELECT COUNT(*) AS total FROM projects  `, (err, response) => {
+    //         if (err) return res.send(err);
+    //         const pages = Math.ceil(response.rows[0].total / limit);
 
 
-            sql = `SELECT * FROM projects LIMIT ${limit} OFFSET ${offset}`;
-            pool.query(sql, (err, response) => {
-                if (err) return res.send(err);
-                res.render('projects/listProject', {
-                    data: response.rows,
-                    page,
-                    query: req.query,
-                    pages
+    //         sql = `SELECT * FROM projects LIMIT ${limit} OFFSET ${offset}`;
+    //         pool.query(sql, (err, response) => {
+    //             if (err) return res.send(err);
+    //             res.render('projects/listProject', {
+    //                 data: response.rows,
+    //                 page,
+    //                 query: req.query,
+    //                 pages
                     
 
-                });
-            });
-        });
-    });
-    /* GET home page. */
-    // router.get('/', (req, res, next) => {     
-    //     let result = [];
-    //     let filterData = false;
-
-    //     if (req.query.check_id && req.query.id) {
-    //         result.push(`projects.projectid = ${req.query.id}`);
-    //         filterData = true;
-    //     }
-    //     if (req.query.check_name && req.query.name) {
-    //         result.push(`projects.name ILIKE '%${req.query.name.toLowerCase()}%`);
-    //         filterData = true;
-    //     }
-    //     if (req.query.check_member && req.query.member) {
-    //         result.push(`members.userid = '${req.query.member}'`);
-    //         filterData = true;
-    //         console.log(req.query);
-    //     }
-
-    //     let sql = `SELECT COUNT(*) AS total FROM projects `;
-    //     if (filterData) {
-    //         sql += ` WHERE ${result.join(' AND ')}`
-    //     }
-    // //     // sql += `) AS projectmember`;
-
-    // //     //todo: PAGINATION
-    //     pool.query(sql, (err, count) => {
-    //         const page = req.query.page || 1;
-    //         const limit = 3;
-    //         const offset = (page - 1) * limit;
-    //         const url = req.url == '/' ? '/?page=1' : req.url;
-
-    //         // console.log('page: ==>' + req.query.page);           
-    //         const total = count.rows[0].total;
-    //         const pages = Math.ceil(total / limit);
-
-    //         let sql = `SELECT FROM projects`;
-    //         if (filterData) {
-    //             sql += ` WHERE ${result.join(' AND ')}`
-    //         }
-    //         sql += ` ORDER BY projectid LIMIT ${limit} OFFSET ${offset}`;
-
-    //         pool.query(sql, (err, row) => {
-    //             res.render('projects/listProject', {
-    //                 data: row.rows,
-    //                 page,
-    //                 pages,
-    //                 query: req.query,
-    //                 url,
-    //                 user: req.session.user
     //             });
     //         });
     //     });
     // });
 
+    router.get("/", (req, res, next) => {
+
+        // filter mode
+        let params = [];
+        if (req.query.check_id && req.query.id) {
+            params.push(`projectid = '${req.query.id}' `)
+
+        }
+        if (req.query.check_name && req.query.name) {
+            params.push(`name ILIKE '%${req.query.name}%' `)
+
+        }
+
+
+        //Pagination
+        const page = req.query.page || 1;
+        let url = req.url == '/' ? '/?page=1' : req.url;
+        const limit = 5;
+        const offset = (page - 1) * limit;
+        let sql = `SELECT COUNT(*) AS total FROM projects  `;
+        //check params filter
+        
+        // if (params.length > 0) {
+        //     sql += `WHERE ${params.join(' AND ')}`
+        // }
+
+        pool.query(sql, (err, response) => {
+            if (err) return res.send(err);
+            const pages = Math.ceil(response.rows[0].total / limit);
+            sql = `SELECT * FROM projects `;
+            //check params filter
+            if (params.length > 0) {
+                sql += `WHERE ${params.join(' AND ')}`
+            }
+            sql += ` LIMIT ${limit} OFFSET ${offset}`;
+            console.log(sql);
+            
+            pool.query(sql, (err, response) => {
+                if (err) return res.send(err);
+                res.render('projects/listProject', {
+                    data: response.rows,
+                    query: req.query,
+                    pages,
+                    page,
+                    url
+
+
+                });
+            });
+        });
+    });
+   
 
     router.post('/', (req, res) => {
         let sql = `UPDATE users SET projectsoptions = '${JSON.stringify(req.body)}' WHERE userid = ${req.session.user.userid}`
@@ -99,33 +99,10 @@ module.exports = function (pool) {
         });
     });
 
-    /* GET PROJECT PAGE. */
-    // router.get('/add', (req, res, next) => {
-    //     let sql = `SELECT userid, firstname || ' ' || lastname AS fullname FROM users`;
-    //     pool.query(sql, (err, result) => {
-    //         let sqladmin = `SELECT isadmin FROM users WHERE userid = ${req.session.user.userid}`;
-    //         pool.query(sqladmin, (err, admin) => {
-    //             admin = admin.rows;
-    //             let isadmin = admin[0].isadmin;
-    //             if (err) {
-    //                 return res.send(err)
-    //             };
-    //             res.render('projects/addProject', {
-    //                 title: 'Add Project',
-    //                 path: 'projects',
-    //                 isadmin,
-    //                 users: result.rows,
-    //                 user: req.session.user
-    //             });
-    //         });
-    //     });
-    // });
 
     router.get('/add', (req, res, next) => {
         let sql = `SELECT userid, firstname || ' ' || lastname AS fullname FROM users`;
         pool.query(sql, (err, result) => {
-
-
             if (err) {
                 return res.send(err)
             };
@@ -165,6 +142,10 @@ module.exports = function (pool) {
                 });
             });
         });
+    });
+
+    router.get("/edit/:id", (req, res) =>{
+
     });
 
 
